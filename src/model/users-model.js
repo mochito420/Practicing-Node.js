5a1023bebaec7e6a8ab5c95fb67554de1e0bd81d
Ricardo Leiva <ricardoleiva1806@gmail.com>
2024-07-16 23:04:54 -0300

    adding more logic to users model

--- src/model/users-model.js
+++ src/model/users-model.js
@@ -1,4 +1,6 @@
 import { readData, writeData } from "../utils/json-controller.js";
+import { v4 as uuidv4 } from "uuid";
+import bcry from "bcrypt";
 
 export class UsersModel {
   static async createUsers({ input }) {
@@ -8,13 +10,24 @@ export class UsersModel {
       throw new Error("this username alredy exist");
     }
 
+    if (input.password.length < 6) {
+      throw new Error("password has to be 6 or more characters");
+    }
+
+    if (input.username.includes(" ")) {
+      throw new Error("username should not have spaces");
+    }
+
     if (altuasData.users) {
+      const saltRounds = 10;
+      const hashedPassword = await bcry.hash(input.password, saltRounds);
+
       altuasData.users.push({
         fullname: input.fullname,
         username: input.username,
-        password: input.password,
-        id: altuasData.users.length,
-        createAdd: new Date().toISOString(),
+        password: hashedPassword,
+        id: uuidv4(),
+        createAdd: new Date().toISOString().split("T")[0],
       });
 
       await writeData(altuasData);
@@ -22,4 +35,3 @@ export class UsersModel {
     return { input };
   }
 }
-
